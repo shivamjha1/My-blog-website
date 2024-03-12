53aa012e1b5dd461dbc5be2c9a76005a4111a282
shivamjha1 <shivam.21b0101002@abes.ac.in>
2024-03-13 01:00:22 +0530

    fix

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import date
-posts=[
+all_post=[
     {
         "slug":"hike-in-the-mountains",
         "image":"mountains.jpg",
@@ -60,10 +60,23 @@ posts=[
     }
 ]
 
+def get_date(post):
+    return post.get('date')
+
 def starting_page(request):
-    return render(request, "blog/index.html")
+    sorted_posts=sorted(all_post,key=get_date)
+    latest_posts=sorted_posts[-3:]
+    
+    return render(request, "blog/index.html",{
+        "posts":latest_posts
+    })
 
 def posts(request):    
-    return render(request,"blog/all-posts.html")
+    return render(request,"blog/all-posts.html",{
+        "all_post":all_post
+    })
 def post_detail(request, slug):
-    return render(request, "blog/post-detail.html")
+    identified_post=next(post for post in all_post if post['slug']==slug)
+    return render(request, "blog/post-detail.html",{
+        "post": identified_post
+    })
