2c6e4cb2fe273476adb67b004d69af061387fefb
shivamjha1 <shivam.21b0101002@abes.ac.in>
2024-04-09 20:41:33 +0530

    n

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    firstname=models.CharField(max_length=30)
+    lastname=models.CharField(max_length=30,null=True)
+    email=models.CharField(max_length=30,null=True)
+    
+class Tags(models.Model):
+    caption=models.CharField(max_length=20)
+        
+class blog(models.Model):
+    title=models.CharField(max_length=50,null=False)
+    image=models.CharField(max_length=50)
+    excerpt=models.CharField(max_length=100)
+    date=models.DateField()
+    slug=models.SlugField(default="",blank=True, null=False,db_index=True)
+    content=models.TextField(max_length=500)
+    author=models.ForeignKey(Author,on_delete=models.CASCADE,related_name="blogs",null=True)
+    tags=models.ManyToManyField(Tags,null=True)
