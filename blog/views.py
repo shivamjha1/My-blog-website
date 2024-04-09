4ae5ec5ce84c349f46de759cb07c16815f7808d6
shivamjha1 <shivam.21b0101002@abes.ac.in>
2024-04-09 23:48:13 +0530

    j

--- blog/views.py
+++ blog/views.py
@@ -5,14 +5,18 @@ from datetime import date
 from .models import Author,Blog,Tags
 all_post=Blog.objects.all()
 all_author=Author.objects.all()
-def starting_page(request):
-    # sorted_posts=sorted(all_post)
-    # latest_posts=sorted_posts[-3:]
-    
-    return render(request, "blog/index.html",{
-        "posts":all_post
-    })
 
+def starting_page(request):
+    # Sort blog posts by date
+    sorted_posts = sorted(all_post, key=lambda post: post.date, reverse=True)
+    
+    # Get the latest three posts
+    latest_posts = sorted_posts[:3]
+    
+    return render(request, "blog/index.html", {
+        "posts": latest_posts  # Pass latest_posts instead of all_post
+    })
+    
 def posts(request):    
     return render(request,"blog/all-posts.html",{
         "all_post":all_post
@@ -26,6 +30,5 @@ def post_detail(request, slug):
 #     identified_author = get_object_or_404(Author, firstname=author)
 #     return render(request, "blog/author.html", {"author": identified_author})
 def author(request, author):
-    # Assuming `all_author` is defined in your views as in your previous code
     identified_author = next(auth for auth in all_author if auth.firstname == author)
     return render(request, "blog/author.html", {"author": identified_author})
